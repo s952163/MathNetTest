3c97500ad329e894fb9d84f7c8dd53a7c4280393
s952163 <s995110@yahoo.co.jp>
2017-09-26 11:42:42 +0900

    initial commit

--- MathNetTest/Program.fs
+++ MathNetTest/Program.fs
@@ -1,7 +1,24 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+#if INTERACTIVE
+      #r @"../packages/MathNet.Numerics.FSharp/lib/net40/MathNet.Numerics.FSharp.dll"
+      #r @"../packages/MathNet.Numerics/lib/net40/MathNet.Numerics.dll"
+
+#endif
+
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.MatrixExtensions
+open MathNet.Numerics.LinearAlgebra.Double
+open System
+
 [<EntryPoint>]
 let main argv = 
+    let vec = DenseVector([| 1.0; 2.0; 3.0; 4.0 |])
+    let mat = matrix [[ 1.0; 0.0 ]
+                      [ 0.0; 0.0 ]]
+
+    Console.WriteLine(vec)
+    Console.WriteLine(mat)
     printfn "%A" argv
     0 // return an integer exit code
---
+++ MathNetTest/Script1.fsx
@@ -0,0 +1,15 @@
+﻿
+#r @"../packages/MathNet.Numerics.FSharp/lib/net40/MathNet.Numerics.FSharp.dll"
+#r @"../packages/MathNet.Numerics/lib/net40/MathNet.Numerics.dll"
+
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.MatrixExtensions
+open MathNet.Numerics.LinearAlgebra.Double
+open System
+
+let vec = DenseVector([| 1.0; 2.0; 3.0; 4.0 |])
+let mat = matrix [[ 1.0; 0.0 ]
+                  [ 0.0; 0.0 ]]
+
+Console.WriteLine(vec)
+Console.WriteLine(mat)
